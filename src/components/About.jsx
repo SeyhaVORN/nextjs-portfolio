5aa7da9b8c3b9c108cbcfc1244e6a851d6639420
seyhavorn <vornseyha4758@gmail.com>
2022-08-22 16:38:50 +0700

    add animation with main section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from 'next/image';
 import AboutImg from '../../public/assets/seyha.png';
 import Link from "next/link";
+import {motion, Variant} from "framer-motion";
 
 const About = () => {
   return (
@@ -26,9 +27,12 @@ const About = () => {
           </Link>
         </div>
         <div className='flex justify-center items-center'>
-          <div className='sm:max-w-[80%] w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-            <Image src={AboutImg} className='rounded-xl' alt='/' />
-          </div>
+          <motion.div
+            initial={{x: -100}}
+            animate={{x: 0, rotate: 360}}
+            className='sm:max-w-[80%] w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <Image src={AboutImg} className='rounded-xl' alt='/'/>
+          </motion.div>
         </div>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,68 @@ import Link from 'next/link';
 import React from 'react';
 import {AiOutlineMail} from 'react-icons/ai';
 import {FaFacebook, FaGithub, FaLinkedinIn, FaTelegram} from 'react-icons/fa';
+import {motion, Variant} from "framer-motion";
 
 const Main = () => {
+  const textAnimate = {
+    offscreen: {x: -40, opacity: 0},
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: .5,
+      }
+    }
+  };
+  const textName = {
+    offscreen: {x: 40, opacity: 0},
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: .7,
+      }
+    }
+  };
+  const textHi = {
+    offscreen: {x: -40, opacity: 0},
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: .5,
+      }
+    }
+  };
   return (
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className='uppercase text-sm tracking-widest text-gray-600'>WELCOME TO MY PORTFOLIO</p>
+          <motion.div
+            initial={'offscreen'}
+            animate={'onscreen'}
+            variants={textAnimate}
+          >
+            <p className='uppercase text-sm tracking-widest text-gray-600'>WELCOME TO MY PORTFOLIO</p>
+          </motion.div>
+
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5] font-style'>SEYHA VORN</span>
+
+            <div className='flex justify-center items-center'>
+              <motion.div
+                initial={'offscreen'}
+                animate={'onscreen'}
+                variants={textHi}
+                className='px-4'>Hi, I&#39;m
+              </motion.div>
+              <motion.div className='text-[#5651e5] font-style'
+                          initial={'offscreen'}
+                          animate={'onscreen'}
+                          variants={textName}
+              >SEYHA VORN
+              </motion.div>
+            </div>
+
           </h1>
           <h1 className='py-2 text-gray-700'>A Full Stack Web Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
